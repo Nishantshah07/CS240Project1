40b8bdfc33fe7befcb4f61643490572bfe34024c
Nishantshah07 <139062418+Nishantshah07@users.noreply.github.com>
2026-02-17 13:31:21 -0500

    generateExpression function

--- nishant_functions.cc
+++ nishant_functions.cc
@@ -1,3 +1,21 @@
+//
+// Created by Nishant Shah on 2/10/26.
+//
+#include "nishant_functions.h"
+#include <iostream>
+#include<cstdlib>
+using std::cout;
+using std::endl;
+using std::cin;
 
+
+void generateExpression(int &x, int &y, int &z, char &a, char &b) {
+	x = rand() % (10);
+	y = rand() % (10);
+	z = rand() % (10);
+	char operands[5] = {'+', '-', '*', '/', '%'};
+	a = operands[rand() % 5];
+	b = operands[rand() % 5];
+}
 
 #include "nishant_functions.h"
--- nishant_functions.h
+++ nishant_functions.h
@@ -1,6 +1,4 @@
-
-
 #ifndef NISHANT_FUNCTIONS_H_
 #define NISHANT_FUNCTIONS_H_
-
+void generateExpression(int &x, int &y, int &z, char &a, char &b);
 #endif //NISHANT_FUNCTIONS_H_
